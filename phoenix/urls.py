92fe098ffdd2daaa15d6a479f46dd61bf150ceec
ojs87 <73496902+ojs87@users.noreply.github.com>
2021-04-07 09:46:44 +0100

    added users

--- phoenix/urls.py
+++ phoenix/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
     path('items', views.items, name='items'),
     path('quests/<str:quest>', views.quests, name='quests'),
     path('foundinraid', views.foundinraid, name='foundinraid'),
